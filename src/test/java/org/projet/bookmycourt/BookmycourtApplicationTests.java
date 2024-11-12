0f8f89f4ce93ef4b6b9d435fb94f1d49c24cecb5
lamaachi <Najia@brim666>
2024-11-12 16:35:02 +0100

    first commit

---
+++ src/test/java/org/projet/bookmycourt/BookmycourtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projet.bookmycourt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmycourtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
