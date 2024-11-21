0a54d34882336f5638cfe652bb07ef5e975c94b1
lamaachi <istatasila7@gmail.com>
2024-11-21 16:26:27 +0000

    set the first set up of docker-compose.yml

--- centre-service/src/main/java/org/projet/centreservice/CentreServiceApplication.java
+++ centre-service/src/main/java/org/projet/centreservice/CentreServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CentreServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CentreServiceApplication.class, args);
     }
-
 }
