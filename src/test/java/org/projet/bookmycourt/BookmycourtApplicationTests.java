1e6c181acf2715a9712f5b70192fb41681aa77d4
ismailtarik <tarikismail600@gmail.com>
2024-11-28 19:51:25 +0100

    Commit the register service

--- src/test/java/org/projet/bookmycourt/BookmycourtApplicationTests.java
+++ src/test/java/org/projet/bookmycourt/BookmycourtApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookmycourtApplicationTests {
+public class BookmycourtApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
