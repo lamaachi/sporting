f934c27db5697cf7a9e0978e3385f6680d57e65e
ismailtarik <tarikismail600@gmail.com>
2024-11-30 00:07:18 +0100

    Commit the service auth

---
+++ auth-service/src/main/java/org/projet/registryservice/JwtConverter.java
@@ -0,0 +1,66 @@
+package org.projet.registryservice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
+@RequiredArgsConstructor
+public class JwtConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+    private final JwtConverterProperties jwtConverterProperties;
+
+    @Override
+    public AbstractAuthenticationToken convert(Jwt jwt) {
+        Collection<GrantedAuthority> authorities = Stream.concat(
+                jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
+                extractResourceRoles(jwt).stream()
+        ).collect(Collectors.toSet());
+
+        return new JwtAuthenticationToken(jwt, authorities, principalClaimName(jwt));
+    }
+
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+        if (resourceAccess == null) {
+            return Set.of();
+        }
+
+        Map<String, Object> resource = (Map<String, Object>) resourceAccess.get(jwtConverterProperties.getResourceId());
+        if (resource == null) {
+            return Set.of();
+        }
+
+        Collection<String> resourceRoles = (Collection<String>) resource.get("roles");
+        if (resourceRoles == null) {
+            return Set.of();
+        }
+
+        return resourceRoles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
+    }
+
+    private String principalClaimName(Jwt jwt) {
+        String claimName = JwtClaimNames.SUB;
+
+        if (jwtConverterProperties.getPrincipalAttribute() != null) {
+            claimName = jwtConverterProperties.getPrincipalAttribute();
+        }
+        return jwt.getClaim(claimName);
+    }
+}
