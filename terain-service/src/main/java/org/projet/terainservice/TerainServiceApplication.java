c0ad5d28adabedce309fbf41add8a7ab148829e6
lamaachi <Najia@brim666>
2024-12-03 14:58:31 +0100

    add terain-service and fix pom packaging in parent module

---
+++ terain-service/src/main/java/org/projet/terainservice/TerainServiceApplication.java
@@ -0,0 +1,13 @@
+package org.projet.terainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerainServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerainServiceApplication.class, args);
+    }
+
+}
---
+++ terain-service/src/test/java/org/projet/terainservice/TerainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projet.terainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerainServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
